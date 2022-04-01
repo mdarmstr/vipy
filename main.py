a43d64ce8bce38982b396818bc57218d4bc63357
mdarmstr <mdarmstr@ualberta.ca>
2022-04-01 09:37:20 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from sklearn.cross_decomposition import PLSRegression
+
+
+def vipy(X, y, ncomponents):
+
+    pls = PLSRegression(n_components=ncomponents)
+    (xscrs, yscrs) = pls.fit_transform(X, y)
+    xw = pls.x_weights_
+    xlds = pls.x_loadings_
+    b = np.linalg.pinv(xscrs) @ y
+    xw = (xw / np.linalg(xw, axis=0)) ** 2
+    sz = np.size(X, 1)
+    vipscrs = []
+
+    for vrbl in range(sz):
+        nmtr = np.sum((b**2) @ xscrs.T @ xscrs @ xw[vrbl, :])
+        dmtr = np.sum((b**2) @ xscrs.T @ xscrs)
+        vipscrs.append(np.sqrt((sz * nmtr) / dmtr))
+
+    return vipscrs
+
+
+
